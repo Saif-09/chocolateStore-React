f3c08f914e711bdb3546c94e0f1a5623f17e6d83
Saif-09 <saifmd238@gmail.com>
2023-09-15 05:18:00 +0530

    Added commments

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,51 +1,60 @@
+//I have used ContextAPI in  my code to make code cleaner and maintainable, context api make it easy to handle state management, I have to update the value of cart, total price and individual quantity of each chocolate, so i used context API.
+//When the user selects the chocolate then he will not add more than 8 chocolates in the cart, as per requirement in assesment i have made functionality that when user add more than 8 chocolate then it will give alert that user cannot add more than this.
 import { createContext, useState, useContext } from "react";
 
+// Create a context for storing cart-related data
 const itemContext = createContext();
 
+// Custom hook to access the context's value
 const useValue = () => {
-    const value = useContext(itemContext)
+    const value = useContext(itemContext);
     return value;
 }
 
+// Context provider component for managing cart state
 const ChocoContext = ({ children }) => {
-
+    // State variables to manage cart data
     const [total, setTotal] = useState(0);
     const [item, setItem] = useState(0);
     const [cart, setCart] = useState([]);
-    const [qty, SetQty] = useState(0)
+    const [qty, SetQty] = useState(0);
 
+    // Function to add a product to the cart
     const handleAdd = (prod) => {
-        if(cart.length<8){
-        console.log('inside handleAdd');
-        console.log('first',cart)
-        const index = cart.findIndex((item) => item.id === prod.id);
-        console.log('second',cart)
-        console.log('index',index)
-        
+        if (cart.length < 8) {
+            console.log('inside handleAdd');
+            console.log('first', cart);
 
-        if (index === -1) {
-            setCart([...cart, { ...prod, qty: 1 }]);
-            setTotal(total + prod.price);
-        } else if(cart.reduce((acc, item) => acc + item.qty, 0) < 8){
-            const updatedCart = [...cart];
-            updatedCart[index].qty++;
-             SetQty(updatedCart[index].qty)
-            setCart(updatedCart);
-            setTotal(total + prod.price);
-        }else {
-            
+            // Find the index of the product in the cart
+            const index = cart.findIndex((item) => item.id === prod.id);
+            console.log('second', cart);
+            console.log('index', index);
+
+            if (index === -1) {
+                // If the product is not in the cart, add it with a quantity of 1
+                setCart([...cart, { ...prod, qty: 1 }]);
+                setTotal(total + prod.price);
+            } else if (cart.reduce((acc, item) => acc + item.qty, 0) < 8) {
+                // If the product is already in the cart, increment its quantity
+                const updatedCart = [...cart];
+                updatedCart[index].qty++;
+                SetQty(updatedCart[index].qty);
+                setCart(updatedCart);
+                setTotal(total + prod.price);
+            } else {
+                // If the custom pack is full, show an alert
+                alert('Custom pack is full. You cannot add more items.');
+                return;
+            }
+            setItem(item + 1);
+        } else {
+            // If the custom pack is full, show an alert
             alert('Custom pack is full. You cannot add more items.');
-            return
+            return;
         }
-        setItem(item + 1);
-    }else{
-        alert('Custom pack is full. You cannot add more items.');
-        return
     }
 
-}
-
-
+    // Function to remove a product from the cart
     const handleRemove = (prod) => {
         const index = cart.findIndex((item) => item.id === prod.id);
 
@@ -61,13 +70,13 @@ const ChocoContext = ({ children }) => {
         }
     };
 
-
-
+    // Provide cart-related data to child components using the context
     return (
-        <itemContext.Provider value={{ total, item, handleAdd, handleRemove,qty ,cart}}>
+        <itemContext.Provider value={{ total, item, handleAdd, handleRemove, qty, cart }}>
             {children}
         </itemContext.Provider>
     )
 }
+
 export default ChocoContext;
 export { useValue };
--- src/components/ChocolateCard.jsx
+++ src/components/ChocolateCard.jsx
@@ -2,17 +2,24 @@ import React from 'react';
 import { useValue } from '../Context';
 
 const ChocolateCard = ({ name, price, image_url, id }) => {
+    // Get the context values for adding and removing items from the cart
     const { handleAdd, handleRemove, cart } = useValue();
+
+    // Find the quantity of this chocolate in the cart
     const qty = cart.find((item) => item.id === id)?.qty;
 
     return (
         <div className="bg-white rounded-lg shadow-lg p-4 transition-transform transform hover:scale-105">
+            {/* Display the chocolate's image */}
             <div className="w-full h-48 mx-auto overflow-hidden">
                 <img src={image_url} alt={name} className="w-full h-full object-cover" />
             </div>
+            {/* Display the chocolate's name */}
             <div className="text-lg font-semibold mt-4 text-gray-800">{name}</div>
+            {/* Display the chocolate's price */}
             <div className="text-gray-800 mt-1">&#x20B9; {price.toFixed(2)}</div>
             <div className="flex justify-between mt-4">
+                {/* Button to add the chocolate to the cart */}
                 <button
                     className="bg-orange-500 hover:bg-orange-400 text-white px-4 py-2 rounded font-semibold"
                     onClick={() => handleAdd({ price, id, qty })}
@@ -20,12 +27,14 @@ const ChocolateCard = ({ name, price, image_url, id }) => {
                     Add
                 </button>
 
+                {/* Display the quantity of this chocolate in the cart */}
                 {qty > 0 && (
                     <div className="bg-orange-500 text-white px-3 py-2 rounded font-semibold">
                         Qty: {qty}
                     </div>
                 )}
 
+                {/* Button to remove the chocolate from the cart */}
                 <button
                     className="bg-red-500 hover:bg-red-400 text-white px-4 py-2 rounded font-semibold"
                     onClick={() => handleRemove({ id })}
